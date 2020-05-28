36310e1059f0ebda1e3e03cf37af648f7d057195
Brad V <shiva@shivabeach.com>
2020-05-28 15:39:38 -0400

    Herbal.js finaly works

--- assets/js/herbal.js
+++ assets/js/herbal.js
@@ -3,11 +3,19 @@
  * make addtolist function then Display costs
  * Not making the connection between the storage and the display
  */
+const remove = document.querySelector('.delete');
 const herb = document.getElementById("herbal");
 let herbs;
 let storedData = localStorage.getItem("herbs");
 if (storedData !== null) {
   herbs = JSON.parse(storedData);
+} else {
+  herbs = [];
+}
+
+function herbal() {
+  constructor(needs)
+    this.needs = needs;
 }
 // works (addCosts)
 function storeNeeds(needs) {
@@ -26,9 +34,10 @@ function display() {
 function addToList(herbs) {
   const list = document.querySelector('.disp');
   const li = document.createElement('li');
+  li.classList.add('under');
 
   li.innerHTML = `
-    ${herbs} `;
+    ${herbs}  - <span><a class="delete" href="#">X</a></span>`;
     list.appendChild(li);
 }
 //getCosts()
@@ -36,6 +45,24 @@ function getItems() {
   herbs = JSON.parse(localStorage.getItem('herbs'));
   return herbs;
 }
+
+function deleteNeed(el) {
+  if (el.classList.contains('delete')) {
+			el.parentElement.parentElement.remove();
+	}
+}
+
+function removeNeed(e) {
+  const del = getItems(herbs);
+  del.forEach((needs, index) => {
+    if(needs === needs) {
+      del.splice(index, 1);
+    }
+  });
+  localStorage.setItem('herbs', JSON.stringify(herbs));
+  console.log(herbs);
+}
+
 addEventListener('DOMContentLoaded', () => {
   display();
 })
@@ -46,4 +73,9 @@ herb.addEventListener("submit", (e) => {
   const needs = document.getElementById('needs').value;
   storeNeeds(needs);
   addToList(needs);
+})
+document.querySelector('.disp').addEventListener('click', (e) => {
+  e.target;
+  deleteNeed(e.target);
+  removeNeed(e.target.parentElement.parentElement.textContent);
 })
